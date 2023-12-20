d015c4dee4f016937adeefefdfbd047b81c4781c
Valorgoing <1575012869@qq.com>
2023-12-20 15:58:07 +0800

    首次提交

---
+++ src/main/java/com/example/demo/HelloResource.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+    public String hello1() {
+        return "Hello, 我是张三!";
+    }
+}
